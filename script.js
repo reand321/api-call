fcff8b6140d4ba5d5b4811c81c9e9ffa59c14bf5
reand321 <johndelrosario123@gmail.com>
2024-11-11 19:02:18 +0800

    Everything is fixed, minor changes may occur

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function playSound() {
-    const audio = new Audio('meow.mp3'); // Updated path
+    const audio = new Audio('assets/meow.mp3'); // Updated path
     audio.play().catch(error => {
         console.error("Error playing sound:", error);
     });
